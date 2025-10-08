c45736ca715219fb5c2344e3dfb2ebec7edf036f
pranjal tiwari <Pranjalttiwarii@gmail.com>
2025-10-08 13:37:44 +0530

    update file

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
 DEBUG = os.getenv('DEBUG', '1') == '1'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
